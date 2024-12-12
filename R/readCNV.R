aa148212662d99944aea3b2f54c1f12653471c6d
MaximiliaEggle <maximilia.eggle@dkfz-heidelberg.de>
2024-12-12 16:31:24 +0100

    Changing read.delim to fread and adding format_cnv_data function for reading cnv.info and assuming column index if not provided

--- R/readCNV.R
+++ R/readCNV.R
@@ -35,8 +35,118 @@ readCNV <- function(cn.info = NULL, chr.col = NULL, start.col = NULL, end.col = 
   if(is.null(cn.info) || is.na(cn.info)){
     stop("Error: missing cn.info! Please provide path to file with copy number information.")
   }
+  estimate.alleles <- FALSE # will be set to TRUE if allele info is not provided, see below
 
-  cn.info = read.delim(cn.info, sep="\t", header = TRUE)
+  ## Read cn.info and assume column index if not provided
+  cn.info <- .format_cnv_data(cn.info = cn.info, chr.col = chr.col, start.col = start.col, end.col = end.col, A.col = A.col, B.col = B.col, tcn.col = tcn.col)
+
+  message("********** Read in ", nrow(cn.info), " segments with copy number information on ", length(unique(cn.info[[chr.col]])), " chromosomes.")
+
+  ## round copy number to multiples of 0.2
+  cn.info[[tcn.col]] <- as.numeric(as.character(cn.info[[tcn.col]]))
+  ## for callers without subconal/clonal assignment, remove subclonals based on maximal deviation of 0.2
+  cn.info <- cn.info[cn.info[[tcn.col]]%%1 <=  0.2 | cn.info[[tcn.col]]%%1 >= 0.8,,drop = FALSE]
+  ## round the remaining copy numbers
+  cn.info[[tcn.col]] <- round(cn.info[[tcn.col]])
+
+  message("********** Removing ", sum(is.na(cn.info[[tcn.col]])), " segments without copy number information...")
+
+  cn.info <- cn.info[!is.na(cn.info[[tcn.col]]),]
+
+  if(nrow(cn.info)==0){
+    stop("Error: no segments with copy number information provided.")
+  }
+
+  message("********** Removing ", sum(cn.info[[tcn.col]] > max.cn), " segments with copy number > ", max.cn, "...")
+
+  cn.info <- cn.info[cn.info[[tcn.col]] <= max.cn & cn.info[[tcn.col]] > 0,]
+
+  if(nrow(cn.info)==0){
+    stop("Error: no segments with copy number information greater 0 and <= ", max.cn)
+  }
+
+  if(estimate.alleles){ # assume 1:1, 2:1, 2:2, ... configuration
+    cn.info <- .estimate_alleles(cn.info, tcn.col)
+  }
+
+  ## in ACEseq output A and B alleles are characters - transform to numeric.
+  ## moreover, round copy numbers to 1 digit (for e.g. input from PURPLE)
+  cn.info[[A.col]] <- as.numeric(as.character(cn.info[[A.col]]))
+  cn.info[[B.col]] <- as.numeric(as.character(cn.info[[B.col]]))
+
+  ## for callers without subconal/clonal assignment, remove subclonals based on maximal deviation of 0.2 for the total CN (A+B), and based on a maximal deviation of 0.1 for each allele
+  cn.info <- cn.info[cn.info[[A.col]]%%1 <=  0.1 | cn.info[[A.col]]%%1 >= 0.9,,drop = FALSE]
+  cn.info <- cn.info[cn.info[[B.col]]%%1 <=  0.1 | cn.info[[B.col]]%%1 >= 0.9,,drop = FALSE]
+  ## round the remaining copy numbers
+  cn.info[[A.col]] <- round(cn.info[[A.col]])
+  cn.info[[B.col]] <- round(cn.info[[B.col]])
+
+
+  ## check chromosome format and amend if not 'chr1', 'chr2', etc.
+  if(grepl("chr", cn.info[1, ..chr.col])){
+    message("********** Change chromosome names to 1, 2, 3, ...")
+    cn.info[[chr.col]] <- gsub(pattern = "chr", replacement = "", x = cn.info[[chr.col]])
+  }
+
+  ## subset on autosomes
+  if(ignore.XY){
+    message("********** Removing ", sum(cn.info[[chr.col]] %in% c("X", "Y")), " segments on allosomes...")
+    cn.info <- cn.info[!cn.info[[chr.col]] %in% c("X", "Y"),]
+    if(nrow(cn.info)==0){
+      stop("No copy number information on autosomes!")
+    }
+  }
+
+  ## order and re-name columns
+  cn.info <- cn.info[, .(Chr = cn.info[[chr.col]],
+                         Start = cn.info[[start.col]],
+                         End = cn.info[[end.col]],
+                         A = cn.info[[A.col]],
+                         B = cn.info[[B.col]])]
+
+
+  ## filter NA-segments
+  message("********** Removing ", sum(is.na(cn.info[["A"]]) | is.na(cn.info[["B"]])) , " segments without B allele information...")
+  cn.info <- cn.info[!is.na(cn.info[["A"]]) & !is.na(cn.info[["B"]])]
+
+  ## add total copy number
+  cn.info$TCN <- cn.info$A + cn.info$B
+
+  ## merge adjacent segments
+  message("********** Merging adjacent segments with equal copy number...")
+  cn.info <- .merge_adjacent_segs(cn.info, merge.tolerance)
+  message("********** Retaining ", nrow(cn.info), " segments with copy number information on ", length(unique(cn.info$Chr)), " chromosomes.")
+
+  if(any(cn.info$End < cn.info$Start)){
+    warning("Removing ", sum(cn.info$End < cn.info$Start), " segments with start > end")
+    cn.info <- cn.info[cn.info$Start <= cn.info$End,]
+  }
+  if(sum(cn.info$End - cn.info$Start) < 3*10^8){
+    warning("Less than 10% of the genome with valid copy number information.")
+  }
+
+  attr(cn.info, "ID") <- tumor.id
+
+  return(cn.info)
+
+}
+
+.merge_adjacent_segs <- function(cn.info, merge.tolerance){
+  cn.info <- split(cn.info, cn.info$Chr) # split by chromosome
+  cn.info <- lapply(cn.info, function(x){
+    to.keep <- 1 + which(!(x$Start[-1] - x$End[-length(x$End)] < merge.tolerance & x$A[-1] == x$A[-length(x$A)] & x$B[-1] == x$B[-length(x$B)])) # merge segments if they have the same allele counts and if start and end are less than the merge.tolerance apart from each other
+    end <- c(x$End[to.keep - 1], x$End[length(x$End)])
+    x <- x[c(1, to.keep),]
+    x$End <- end
+    x
+  })
+  cn.info <- do.call(rbind.data.frame, cn.info)
+  return(cn.info)
+}
+
+.format_cnv_data <- function(cn.info = cn.info, chr.col = NULL, start.col = NULL, end.col = NULL, A.col = NULL, B.col = NULL, tcn.col = NULL) {
+  cn.info <- data.table::fread(file = cn.info, sep = "\t", header = TRUE)
+
 
   if(is.null(chr.col) || is.na(chr.col)){
     chr.col <- colnames(cn.info)[grepl("chr", colnames(cn.info), ignore.case = T)] # try to match with standard nomenclature
@@ -80,7 +190,6 @@ readCNV <- function(cn.info = NULL, chr.col = NULL, start.col = NULL, end.col = 
     stop("Error: 'arg' should be string or numeric.")
   }
 
-  estimate.alleles <- FALSE # will be set to TRUE if allele info is not provided, see below
 
   if(is.null(A.col) || is.na(A.col)){
     A.col <- colnames(cn.info)[grepl("major", colnames(cn.info), ignore.case = TRUE)] # try to match with standard nomenclature
@@ -89,7 +198,7 @@ readCNV <- function(cn.info = NULL, chr.col = NULL, start.col = NULL, end.col = 
         A.col <- "A"  # assume standard nomenclature
         warning("A allele identifier not provided, assuming 'A'")
       }else if (!is.null(tcn.col) & !is.null(B.col)){
-        cn.info$A <- cn.info[,tcn.col] - cn.info[,B.col]
+        cn.info$A <- cn.info[[tcn.col]] - cn.info[[B.col]]
         message("********** A allele identifier not provided, computing A = TCN - B.")
       }else{
         estimate.alleles <- TRUE # Assume alleles as standard 1:1, 2:1 or 2:2 configuration
@@ -117,7 +226,7 @@ readCNV <- function(cn.info = NULL, chr.col = NULL, start.col = NULL, end.col = 
         B.col <- "B" # assume standard nomenclature
         warning("B allele identifier not provided, assuming 'B'")
       }else if (!is.null(tcn.col) & !estimate.alleles){
-        cn.info$B <- cn.info[,tcn.col] - cn.info[,A.col]
+        cn.info$B <- cn.info[[tcn.col]] - cn.info[[A.col]]
         message("********** B allele identifier not provided, computing B = TCN - A.")
       }else{
         B.col <- "B"
@@ -138,7 +247,7 @@ readCNV <- function(cn.info = NULL, chr.col = NULL, start.col = NULL, end.col = 
 
   if(is.null(tcn.col) || is.na(tcn.col)){
     if(!is.null(A.col) & !is.null(B.col)){
-      cn.info$TCN <- as.numeric(cn.info[,A.col]) + as.numeric(cn.info[,B.col])
+      cn.info$TCN <- as.numeric(cn.info[[A.col]]) + as.numeric(cn.info[[B.col]])
       tcn.col <- "TCN"
       message("********** Total copy number computed as A + B.")
     }else{
@@ -160,119 +269,22 @@ readCNV <- function(cn.info = NULL, chr.col = NULL, start.col = NULL, end.col = 
     stop("Error: 'arg' should be string or numeric.")
   }
 
-  if(!(is.character(cn.info[,chr.col]) | is.numeric(cn.info[,chr.col]))){
+  if(!(is.character(cn.info[[chr.col]]) | is.numeric(cn.info[[chr.col]]))){
     stop("Error: chromosome information must be string or numeric.")
-  }else if(!(is.character(cn.info[,tcn.col]) | is.numeric(cn.info[,tcn.col]))){
+  }else if(!(is.character(cn.info[[tcn.col]]) | is.numeric(cn.info[[tcn.col]]))){
     stop("Error: total copy number must be string or numeric.")
-  }else if(!is.numeric(cn.info[,start.col])){
+  }else if(!is.numeric(cn.info[[start.col]])){
     stop("Error: start position must be numeric.")
-  }else if(!is.numeric(cn.info[,end.col])){
+  }else if(!is.numeric(cn.info[[end.col]])){
     stop("Error: end position must be numeric.")
   }
 
-  message("********** Read in ", nrow(cn.info), " segments with copy number information on ", length(unique(cn.info[,chr.col])), " chromosomes.")
-
-  ## round copy number to multiples of 0.2
-  cn.info[,tcn.col] <- as.numeric(as.character(cn.info[,tcn.col]))
-  ## for callers without subconal/clonal assignment, remove subclonals based on maximal deviation of 0.2
-  cn.info <- cn.info[cn.info[,tcn.col]%%1 <=  0.2 | cn.info[,tcn.col]%%1 >= 0.8,,drop = FALSE]
-  ## round the remaining copy numbers
-  cn.info[,tcn.col] <- round(cn.info[,tcn.col])
-
-  message("********** Removing ", sum(is.na(cn.info[,tcn.col])), " segments without copy number information...")
-
-  cn.info <- cn.info[!is.na(cn.info[,tcn.col]),]
-
-  if(nrow(cn.info)==0){
-    stop("Error: no segments with copy number information provided.")
-  }
-
-  message("********** Removing ", sum(cn.info[,tcn.col] > max.cn), " segments with copy number > ", max.cn, "...")
-
-  cn.info <- cn.info[cn.info[,tcn.col] <= max.cn & cn.info[,tcn.col] > 0,]
-
-  if(nrow(cn.info)==0){
-    stop("Error: no segments with copy number information greater 0 and <= ", max.cn)
-  }
-
-  if(estimate.alleles){ # assume 1:1, 2:1, 2:2, ... configuration
-    cn.info <- .estimate_alleles(cn.info, tcn.col)
-  }
-
-  ## in ACEseq output A and B alleles are characters - transform to numeric.
-  ## moreover, round copy numbers to 1 digit (for e.g. input from PURPLE)
-  cn.info[,A.col] <- as.numeric(as.character(cn.info[,A.col]))
-  cn.info[,B.col] <- as.numeric(as.character(cn.info[,B.col]))
-
-  ## for callers without subconal/clonal assignment, remove subclonals based on maximal deviation of 0.2 for the total CN (A+B), and based on a maximal deviation of 0.1 for each allele
-  cn.info <- cn.info[cn.info[,A.col]%%1 <=  0.1 | cn.info[,A.col]%%1 >= 0.9,,drop = FALSE]
-  cn.info <- cn.info[cn.info[,B.col]%%1 <=  0.1 | cn.info[,B.col]%%1 >= 0.9,,drop = FALSE]
-  ## round the remaining copy numbers
-  cn.info[,A.col] <- round(cn.info[,A.col])
-  cn.info[,B.col] <- round(cn.info[,B.col])
-
-
-  ## check chromosome format and amend if not 'chr1', 'chr2', etc.
-  if(grepl("chr", cn.info[1,chr.col])){
-    message("********** Change chromosome names to 1, 2, 3, ...")
-    cn.info[,chr.col] <- gsub(pattern = "chr", replacement = "", x = cn.info[,chr.col])
-  }
-
-  ## subset on autosomes
-  if(ignore.XY){
-    message("********** Removing ", sum(cn.info[,chr.col] %in% c("X", "Y")), " segments on allosomes...")
-    cn.info <- cn.info[!cn.info[,chr.col] %in% c("X", "Y"),]
-    if(nrow(cn.info)==0){
-      stop("No copy number information on autosomes!")
-    }
-  }
-
-  ## order and re-name columns
-  cn.info <- cn.info[,c(chr.col, start.col, end.col, A.col, B.col)]
-  colnames(cn.info) <- c("Chr", "Start", "End", "A", "B")
-
-  ## filter NA-segments
-  message("********** Removing ", sum(is.na(cn.info[,"A"]) | is.na(cn.info[,"B"])) , " segments without B allele information...")
-  cn.info <- cn.info[!is.na(cn.info[,"A"]) & !is.na(cn.info[,"B"]),]
-
-  ## add total copy number
-  cn.info$TCN <- cn.info$A + cn.info$B
-
-  ## merge adjacent segments
-  message("********** Merging adjacent segments with equal copy number...")
-  cn.info <- .merge_adjacent_segs(cn.info, merge.tolerance)
-  message("********** Retaining ", nrow(cn.info), " segments with copy number information on ", length(unique(cn.info$Chr)), " chromosomes.")
-
-  if(any(cn.info$End < cn.info$Start)){
-    warning("Removing ", sum(cn.info$End < cn.info$Start), " segments with start > end")
-    cn.info <- cn.info[cn.info$Start <= cn.info$End,]
-  }
-  if(sum(cn.info$End - cn.info$Start) < 3*10^8){
-    warning("Less than 10% of the genome with valid copy number information.")
-  }
-
-  attr(cn.info, "ID") <- tumor.id
-
-  return(cn.info)
-
-}
-
-.merge_adjacent_segs <- function(cn.info, merge.tolerance){
-  cn.info <- split(cn.info, cn.info$Chr) # split by chromosome
-  cn.info <- lapply(cn.info, function(x){
-    to.keep <- 1 + which(!(x$Start[-1] - x$End[-length(x$End)] < merge.tolerance & x$A[-1] == x$A[-length(x$A)] & x$B[-1] == x$B[-length(x$B)])) # merge segments if they have the same allele counts and if start and end are less than the merge.tolerance apart from each other
-    end <- c(x$End[to.keep - 1], x$End[length(x$End)])
-    x <- x[c(1, to.keep),]
-    x$End <- end
-    x
-  })
-  cn.info <- do.call(rbind.data.frame, cn.info)
   return(cn.info)
 }
 
 .estimate_alleles <- function(cn.info, tcn.col){
-  cn.info$A <- ceiling(cn.info[,tcn.col]/2)
-  cn.info$B <- floor(cn.info[,tcn.col]/2)
+  cn.info$A <- ceiling(cn.info[[tcn.col]]/2)
+  cn.info$B <- floor(cn.info[[tcn.col]]/2)
 
   return(cn.info)
 }
