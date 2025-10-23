bde421873c90ac1db4fd5a9cda916abba8d8d04d
MaximiliaEggle <maximilia.eggle@dkfz-heidelberg.de>
2025-10-23 11:17:20 +0200

    Issue #91 Amend expectedClVAF

--- R/estimateClonality.R
+++ R/estimateClonality.R
@@ -85,7 +85,7 @@ estimateClonality <- function(nbObj = NULL, mrcaObj = NULL, ID = NULL,
         if (is.na(CN) || is.na(VAF) || CN == 0 || depth == 0) {
             "n.d."
         } else {
-            expectedVAFs <- .expectedClVAF(CN, purity)
+            expectedVAFs <- .expectedClVAFAB(A, B, purity)
 
             expected_SC <- expectedVAFs[1] * 0.5
             expected_C <- expectedVAFs[1]
@@ -160,8 +160,8 @@ estimateClonality <- function(nbObj = NULL, mrcaObj = NULL, ID = NULL,
     return(snvClonality)
 }
 
-.expectedClVAF <- function(CN, purity) {
-    seq_len(CN) * purity / (purity * CN + 2 * (1 - purity))
+.expectedClVAFAB <- function(A, B, purity) {
+    unique(c(1,B,A) * purity / (purity * (A + B) + 2 * (1 - purity)))
 }
 
 #' Plotting assigned clonality status for every SNV by chromosome
